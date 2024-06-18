88eb83b494beaafed51fa7d70a20324953f648cb
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-18 16:49:42 +0900

    Update and rename entities.js to life.js

---
+++ js/life.js
@@ -0,0 +1,15 @@
+game.PlayerLife = me.Life.extend({
+    init: function(x, y) {
+        var settings = {};
+        settings.image = 'プレイヤー';
+        settings.width = 230;
+        settings.height = 200;
+
+        this._super(me.Life, 'init', [x, y, settings]);
+        this.alwaysUpdate = true;
+        this.maxAngleRotation = Number.prototype.degToRad(-20);
+        this.maxAngleRotationDown = Number.prototype.degToRad(20);
+        // this.renderable.addAnimation("swim", [0, 1, 2]);
+        // this.renderable.setCurrentAnimation("swim");
+    },
+});
