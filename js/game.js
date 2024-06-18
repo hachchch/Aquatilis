ba113b9431fd2041f17c317e45e772f281ea18b9
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-18 16:57:39 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,15 @@
-
+var game = {
+    data: {
+        score : 0,
+        start: false,
+        newHiScore: false,
+        muted: false
+    },
+  
+  resources: [
+            /*画像*/
+        {name: "プレイヤー", type:"image", src: "data/img/プレイヤー1.png"},
+        /*音声*/
+        {name: "theme", type: "audio", src: "data/bgm/"}
+    ],
+};
