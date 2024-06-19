c4be1b75ba6191a2ee3c73b8fea9bff2f37970ba
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-19 10:52:35 +0900

    Update render.js

--- js/render.js
+++ js/render.js
@@ -5,15 +5,3 @@ const image = document.getElementById("resources");
 image.addEventListener("load", (e) => {
   ctx.drawImage(player, 100, 100);
 });
-
-// Moved square
-ctx.translate(110, 30);
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 80, 80);
-
-// Reset current transformation matrix to the identity matrix
-ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-// Unmoved square
-ctx.fillStyle = "gray";
-ctx.fillRect(0, 0, 80, 80);
