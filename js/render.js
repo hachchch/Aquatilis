6b6c919c8826cf4bbbf2f199c748206368f4a6db
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-07-06 09:45:04 +0900

    Delete js/render.js

--- js/render.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const image = document.getElementById("resources");
-
-image.addEventListener("load", (e) => {
-  ctx.drawImage(player, 100, 100);
-});
